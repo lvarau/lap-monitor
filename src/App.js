b02e041bf70ff9ca1234dd771aa825618e0b4898
leonardo <leonardo.victorufpa@gmail.com>
2020-08-22 16:09:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './styles.css'
 
-function App() {
+import ShowLaps from './components/ShowLaps'
+import ShowTime from './components/ShowTime'
+import Button from './components/Button'
+
+const App = () => {
+  const [numLaps, setNumLaps] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    let timer = null
+    if(running) {
+      timer = setInterval(() => {
+        setTime(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if(timer) {
+        clearInterval(timer)
+      }
+    }  
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+  const reset = () => {
+    setNumLaps(0)
+    setTime(0)
+  }
+
+  const incrementLaps = () => {
+    setNumLaps(numLaps + 1)
+  }
+
+  const decrementLaps = () => {
+    if(numLaps > 0) {
+      setNumLaps(numLaps - 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ShowLaps laps={numLaps} />
+      <Button text='+'className='bigger' onClick={incrementLaps}/>
+      <Button text='-'className='bigger' onClick={decrementLaps} />
+
+      {
+        numLaps > 0 &&
+        <ShowTime time={Math.round(time/numLaps)} />
+      }
+      <Button text={running ? 'Pausar' : 'Iniciar'} onClick={toggleRunning}/>
+      <Button text='Reiniciar' onClick={reset}/>
+    </>
   );
 }
 
---
+++ src/components/ShowTime.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ShowTime = (props) => {
+    const time = props.time
+    const minutes = Math.round(time / 60)
+    const seconds = time % 60
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes
+    const secondsStr = seconds < 10 ? '0' + seconds : seconds
+    
+    return (
+      <p className= 'time'>
+          <span>{`${minutesStr}:${secondsStr}`}</span><br />
+          Tempo medio por volta
+      </p>
+    )
+}
+
+export default ShowTime
